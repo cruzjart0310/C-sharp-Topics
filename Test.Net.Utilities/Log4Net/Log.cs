13dc7f2763ede363d20278ad765cd27eeac6268e
Arturo Cruz <cruzjart@gmail.com>
2022-10-13 16:23:50 -0500

    updated

--- Test.Net.Utilities/Log4Net/Log.cs
+++ Test.Net.Utilities/Log4Net/Log.cs
@@ -10,7 +10,6 @@ namespace Test.Net.Utilities.Log4Net
 {
     public static class Log
     {
-        //https://youtu.be/2lAdQ_QwNww
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static void Error(string message)
         {
